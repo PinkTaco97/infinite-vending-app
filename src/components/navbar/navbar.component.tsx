d259d828271035a7be36d144190a7528ed05a7c6
Nathan Robertson <nathanrobertson1997@gmail.com>
2025-07-23 23:54:24 +1000

    refactor: updated navbar,

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -8,31 +8,35 @@ import { ToggleDarkLight } from "@/components/buttons";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center">
-      <div className="bg-background transition-colors duration-300 px-3 min-w-sm max-w-5xl m-5 rounded-full sm:px-3 lg:px-3">
-        <div className="flex items-center justify-between h-16">
-          {/* Left: Logos */}
-          <div className="flex items-center space-x-2">
-            <Image
-              src="/logo-icon.svg"
-              alt="logo icon"
-              className="mx-auto object-contain"
-              width={100}
-              height={50}
-              priority
-            />
-            <Image
-              src="/logo-text.svg"
-              alt="logo text"
-              className="mx-auto object-contain"
-              width={100}
-              height={50}
-              priority
-            />
-          </div>
-          <div className="flex  align-items-center justify-center rounded-full  ">
-            <ToggleDarkLight />
-          </div>
+    <nav className="fixed top-0 left-0 right-0 z-50 w-full p-4">
+      <div className="relative m-auto px-4 max-w-4xl w-full h-16 flex items-center justify-between rounded-full bg-background transition-colors duration-300">
+        {/* Left: Icon Logo */}
+        <div className="flex items-center space-x-2 z-10">
+          <Image
+            src="/logo-icon.svg"
+            alt="logo icon"
+            className="object-contain"
+            width={100}
+            height={50}
+            priority
+          />
+        </div>
+
+        {/* Center: Text Logo */}
+        <div className="absolute left-1/2 transform -translate-x-1/2">
+          <Image
+            src="/logo-text.svg"
+            alt="logo text"
+            className="object-contain"
+            width={200}
+            height={100}
+            priority
+          />
+        </div>
+
+        {/* Right: Theme Toggle */}
+        <div className="z-10">
+          <ToggleDarkLight />
         </div>
       </div>
     </nav>
